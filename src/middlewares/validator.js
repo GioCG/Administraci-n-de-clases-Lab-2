8f04bb2ba07a365135967dc26e397416b62765c2
GioCG <gcarrera-2020313@kinal.edu.gt>
2025-02-17 18:37:34 -0600

    se agregaron las validaciones

---
+++ src/middlewares/validator.js
@@ -0,0 +1,19 @@
+import {body} from"express-validator";
+import { validarCampos } from "./validar-campos.js";
+import { existenteEmail, esRoleValido } from "../helpers/db-validator.js";
+
+export const registerValidator = [
+    body("name","The name is required").not().isEmpty(),
+    body("email","The email must be valid").isEmail(),
+    body("role").custom(esRoleValido),
+    body("email").custom(existenteEmail),
+    body("password", "La contraseña debe tener minimo 8 caracteres").isLength({min: 8}),    validarCampos
+] ;
+
+export const loginValidator = [
+    body("email").optional().isEmail().withMessage("Enter a valid email address"),
+    body("username").optional().isString().withMessage("Enter a valid username"),
+    body("password", "La contraseña debe tener minimo 8 caracteres").isLength({min: 8}),    validarCampos
+]
+
+
